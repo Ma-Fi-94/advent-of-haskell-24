1b48c4a9aee6b23462efb62acae5af1fb53f0f20
Matthias M. Fischer <33027321+Ma-Fi-94@users.noreply.github.com>
2024-12-04 20:50:20 +0100

    Pt 2 -- nice and easy :)

--- day04/Main.hs
+++ day04/Main.hs
@@ -2,23 +2,17 @@ module Main where
 
 import Data.List (reverse, transpose)
 import Data.Universe.Helpers (diagonals)
+import Utils (countOcc)
 
--- Construct all length-i sublists of a given list.
-sublists :: Int -> [a] -> [[a]]
-sublists i xs
-    | i > length xs = []
-    | otherwise     = (take i xs) : sublists i (tail xs)
+type Grid a = [[a]]
 
+------------
+-- Part 1 --
+------------
 
--- Count how often a list appears as a sublist within another.
-count :: Eq a => [a] -> [a] -> Int
-count ndl = length . filter (==ndl) . sublists (length ndl)
-
-
--- Part 1: Count how often "XMAS" appears in the entire grid.
--- < 2604
-part1 :: [[Char]] -> Int
-part1 grid = sum $ map (count "XMAS") searchspace
+-- Count how often "XMAS" appears in the entire grid.
+part1 :: Grid Char -> Int
+part1 grid = sum $ map (countOcc "XMAS") searchspace
   where
     searchspace = grid
                 ++ map reverse grid
@@ -29,12 +23,38 @@ part1 grid = sum $ map (count "XMAS") searchspace
                 ++ diagonals ((map reverse) grid)
                 ++ map reverse (diagonals ((map reverse) grid))
 
+------------
+-- Part 2 --
+------------
+
+-- Given a grid, extract the element at (i, j). Partial function!
+getCell :: Grid a -> (Int, Int) -> a
+getCell g (i, j) = (g !! i) !! j
+
+
+-- Given a grid, extract all possible X-shaped combinations of cells 
+getAllX :: Grid Char -> [[Char]]
+getAllX g = go 0 0
+  where
+    (n, k) = (length g, length (g !! 0))
+    go i j
+        | i > n - 3 = []
+        | j > k - 3 = go (i + 1) 0
+        | otherwise = map (getCell g) [(i, j), (i, j + 2),
+                                       (i + 1, j + 1),
+                                       (i + 2, j), (i + 2, j + 2)]
+                    : go i (j + 1)
+
 main :: IO ()
 main = do
     fileContents <- readFile "input.txt"
     let grid    = lines $ fileContents
 
     print $ part1 grid
+
+    print $ length
+          . filter (`elem` ["MMASS", "SMASM", "SSAMM", "MSAMS"])
+          $ getAllX grid
 
     print $ "Done."
 
