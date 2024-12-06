d37a6a7aba1cbbb7aae23560d94750ee25d553ac
Matthias M. Fischer <33027321+Ma-Fi-94@users.noreply.github.com>
2024-12-06 11:14:15 +0100

    Part 2 works, albeit a bit slowly. Also, lots of refactoring!

--- day06/Main.hs
+++ day06/Main.hs
@@ -1,9 +1,18 @@
 module Main where
 
-import Data.List (nub)
+import Data.Set (Set)
+import qualified Data.Set as Set
+import Data.List (nub, (\\))
+import Data.Maybe
 import Grid (Coord, enumerate, fromList, Grid, shape)
+import Utils (fst3, snd3, thi3)
 
-type Dir = Char
+type Length    = Int
+type Obstacles = [Coord]
+type GridData  = (Length, Length, Obstacles)
+type Direction = Char
+type State     = (Coord, Direction)
+
 
 -- Parse input into Grid for better extraction of relevant info
 parseInput :: String -> Grid Char
@@ -12,48 +21,81 @@ parseInput = fromList . lines
 
 -- Extract all relevant information from Grid:
 -- Grid dimensions, obstrace positions, starting position and direction.
-extractInfo :: Grid Char -> (Int, Int, [Coord], Coord, Dir)
-extractInfo grid = (n, k, obs, pos, dir)
+extractInfo :: Grid Char -> (GridData, State)
+extractInfo grid = ((n, k, obs), state)
   where
-    (n, k)     = shape grid
-    (pos, dir) = head
-               . filter ((`elem` "><^v") . snd)
-               . enumerate
-               $ grid
-    obs        = map fst
-               . filter ((=='#') . snd)
-               . enumerate
-               $ grid
+    (n, k) = shape grid
+    obs    = map fst
+           . filter ((=='#') . snd)
+           . enumerate
+           $ grid
+    state  = head
+           . filter ((`elem` "><^v") . snd)
+           . enumerate
+           $ grid
 
--- Step forward until OOB and keep track of all visited positions
-part1 :: Int -> Int -> [Coord] -> Coord -> Dir -> [Coord]
-part1 n k obstacles = go
+
+
+-- Take maybe one step, depending on whether we would leave the bounds or not.
+-- Return maybe new position and new direction
+step :: GridData -> State -> Maybe State
+step (n, k, obstacles) ((i, j), d)
+    | i < 0 || j < 0 || i == n || j == k         = Nothing
+    | d == '^' && (i - 1, j) `notElem` obstacles = Just ((i - 1, j), d)
+    | d == '>' && (i, j + 1) `notElem` obstacles = Just ((i, j + 1), d)
+    | d == 'v' && (i + 1, j) `notElem` obstacles = Just ((i + 1, j), d)
+    | d == '<' && (i, j - 1) `notElem` obstacles = Just ((i, j - 1), d)
+    | otherwise                                  = Just ((i, j), d')
   where
-    go (i, j) d
-        | i < 0 || j < 0 || i == n || j == k = []
-        | d == '^' && (i - 1, j) `notElem` obstacles = (i, j) : go (i - 1, j) d
-        | d == '>' && (i, j + 1) `notElem` obstacles = (i, j) : go (i, j + 1) d
-        | d == 'v' && (i + 1, j) `notElem` obstacles = (i, j) : go (i + 1, j) d
-        | d == '<' && (i, j - 1) `notElem` obstacles = (i, j) : go (i, j - 1) d
-        | otherwise                                  = go (i, j) d'
+    d' = case d of {'^' -> '>'; '>' -> 'v'; 'v' -> '<'; '<' -> '^'}
+
+
+-- Execute the complete course until we run out of bounds.
+course :: GridData -> State -> [Coord]
+course gridData ((i, j), d) = case step gridData ((i, j), d) of
+    Nothing     -> []
+    Just state' -> (i, j) : course gridData state'
+
+
+------------
+-- Part 2 --
+------------
+
+-- Cycle detection, using the fact that a state is exactly defined by position and direction.
+isCycle :: GridData -> State -> Bool
+isCycle gridData = go Set.empty
+  where
+    go seen state@((i, j), d)
+        | state `Set.member` seen        = True
+        | step gridData state == Nothing = False
+        | otherwise                      = go seen' state'
       where
-        d' = case d of {'^' -> '>'; '>' -> 'v'; 'v' -> '<'; '<' -> '^'}
+        seen'  = state `Set.insert` seen
+        state' = fromJust $ step gridData state
+
+
 
 
 main :: IO ()
 main = do
-    fileContents              <- readFile "input.txt"
-    let grid                  = parseInput fileContents
-    let (n, k, obs, pos, dir) = extractInfo grid
+    fileContents          <- readFile "input.txt"
+    let grid              = parseInput fileContents
+    let (gridData, state) = extractInfo grid
 
+    -- Part 1: Execute the entire course and count the number of individual cells visited.
+
+    let visited = nub $ course gridData state
+    print $ length visited
+
+    -- Part 2: Try placing an obstacle at every position the guard has visited in Part 1,
+    -- but excluding the starting position. Count number of cyclic courses.
+
+    let candidateObstacles = visited \\ [fst state]
+    let gridDatas = map (\ o -> (fst3 gridData, snd3 gridData, o : thi3 gridData))
+                  $ candidateObstacles
     print $ length
-          . nub
-          $ part1 n k obs pos dir
-
-
-
-
-
+          . filter (\ gd -> isCycle gd state)
+          $ gridDatas
 
     print $ "Done."
 
