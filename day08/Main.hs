167f312dae3ec3c568fb9c304b790b29305479e4
Matthias M. Fischer <33027321+Ma-Fi-94@users.noreply.github.com>
2024-12-08 12:28:28 +0100

    Part 2 -- a bit blunt, but it does the trick!

--- day08/Main.hs
+++ day08/Main.hs
@@ -27,6 +27,11 @@ parseInput s = (inputShape, antennas)
 -- Part 1 --
 ------------
 
+-- Check whether a coordinate pair is in bounds of the grid.
+inBounds :: Size -> Coord -> Bool
+inBounds (n, k) (r, c) = r >= 0 && c >= 0 && r < n && c < k
+
+
 -- Given a pair of antenna positions, calculate the position of the
 -- two antinodes (stored in a list for easier processing later).
 antinodesPair :: (Antenna, Antenna) -> [Coord]
@@ -38,9 +43,31 @@ antinodesPair ((r1, c1), (r2, c2)) = [(r1 - dr, c1 - dc),
 
 -- Given a list of antenna positions, calculate all antinode positions.
 antinodesList :: [Antenna] -> [Coord]
-antinodesList as = concatMap antinodesPair uniquePairs
+antinodesList antennas = concatMap antinodesPair uniquePairs
   where
-    uniquePairs = [(a1, a2) | (a1:a2s) <- tails as, a2 <- a2s]
+    uniquePairs = [(a1, a2) | (a1:a2s) <- tails antennas, a2 <- a2s]
+
+
+------------
+-- Part 2 --
+------------
+
+-- Given a pair of antenna positions, calculate the position of all
+-- the antinodes (stored in a list for easier processing later).
+-- We go `maxsteps` in either direction.
+antinodesPair' :: Int -> (Antenna, Antenna) -> [Coord]
+antinodesPair' maxsteps ((r1, c1), (r2, c2)) = [(r1 + i * dr, c1 + i * dc)
+                                             | i <- [(-maxsteps) .. maxsteps]]
+  where
+    (dr, dc) = (r2 - r1, c2 - c1)
+
+
+-- Given a list of antenna positions, calculate all antinode positions.
+-- We go `maxsteps` in either direction for every pair.
+antinodesList' :: Int -> [Antenna] -> [Coord]
+antinodesList' maxsteps antennas = concatMap (antinodesPair' maxsteps) uniquePairs
+  where
+    uniquePairs = [(a1, a2) | (a1:a2s) <- tails antennas, a2 <- a2s]
 
 
 main :: IO ()
@@ -49,10 +76,17 @@ main = do
     let ((n, k), antennas) = parseInput fileContents
 
     print $ length
-          . filter (\ (r, c) -> r >= 0 && c >= 0 && r < n && c < k)
+          . filter (inBounds (n, k))
           . nub
           . concatMap antinodesList
           $ antennas
 
+    -- For part 2, we set `maxsteps` to the size of the grid, which guarantees
+    -- that we will not miss any position on the grid.
+    print $ length
+          . filter (inBounds (n, k))
+          . nub
+          . concatMap (antinodesList' n)
+          $ antennas
     print $ "Done."
 
