def2ab0414c8a1fed88bafa246b293df98d80636
Matthias M. Fischer <33027321+Ma-Fi-94@users.noreply.github.com>
2024-12-10 11:49:22 +0100

    Update Main.hs
    Surprisingly very easy!

--- day10/Main.hs
+++ day10/Main.hs
@@ -4,7 +4,8 @@ import Data.List (nub)
 import Grid (Coord, Grid, enumerate, fromList, vonNeum)
 import Utils (map2, readDigits)
 
-type Cell = (Coord, Int)
+type Cell  = (Coord, Int)
+type Trail = [Cell]
 
 
 -- Sugar
@@ -14,6 +15,9 @@ getPosition = fst
 
 getElevation :: Cell -> Int
 getElevation = snd
+
+isComplete :: Trail -> Bool
+isComplete = (==9) . getElevation . last
 
 
 -- Given a Grid and a Cell, find all suitable next cells on a trail.
@@ -27,15 +31,43 @@ nextSteps grid (position, elevation) = filter isOneAbove neighbours
 
 
 -- Get the number of all possible terminal cells with a '9',
--- when starting at a given cell.
+-- reachable from a given starting cell.
 nbTerminals :: Grid Int -> Cell -> Int
 nbTerminals grid cell = length . nub . map fst $ go [cell]
   where
     go [] = []
-    go cs = terminalCells ++ go (concatMap (nextSteps grid) nonTerminalCells)
+    go cs = terminalCells ++ go nextCells
       where
         terminalCells    = filter ((==9) . getElevation) cs
         nonTerminalCells = filter ((/=9) . getElevation) cs
+        nextCells        = concatMap (nextSteps grid) nonTerminalCells
+
+
+------------
+-- Part 2 --
+------------
+
+-- Given a Grid and a Trail, find all possible trails that emerge
+-- from taking one single step onto a suitable (see above) next cell.
+trailContinuations :: Grid Int -> Trail -> [Trail]
+trailContinuations grid trail = [trail ++ [next] |
+                                 next <- nextSteps grid (last trail)]
+
+
+
+-- Given a starting position, find all possible trails ending
+-- in a nine.
+nbAllTrails :: Grid Int -> Cell -> Int
+nbAllTrails grid cell = length . nub $ go [[cell]]
+  where
+    go [] = []
+    go ts = completeTrails ++ go growingTrails
+      where
+        completeTrails   = filter isComplete ts
+        incompleteTrails = filter (not . isComplete) ts
+        growingTrails    = concatMap (trailContinuations grid) incompleteTrails
+
+
 
 main :: IO ()
 main = do
@@ -44,6 +76,8 @@ main = do
     let startCells = filter ((==0) . getElevation) $ enumerate grid
 
     print $ sum $ map (nbTerminals grid) startCells
+
+    print $ sum $ map (nbAllTrails grid) startCells
 
     print $ "Done."
 
