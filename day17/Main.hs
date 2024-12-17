f837d0212a9577fd43d651dc127de7f19993b28e
Matthias M. Fischer <33027321+Ma-Fi-94@users.noreply.github.com>
2024-12-17 23:07:08 +0100

    Fit for purpose!

--- day17/Main.hs
+++ day17/Main.hs
@@ -5,14 +5,15 @@ import Utils ((!?))
 
 -- Sugar
 type IP           = Int
-type Register     = Int
+type Register     = Integer
 type State        = (Register, Register, Register, IP)
-type Instructions = [Int]
+type Instructions = [Integer]
 
 
--- Run the entire list of instructions. Output is returned as list of ints.
-run :: Instructions -> State -> [Int]
-run instructions = go
+-- Run the entire list of instructions given a starting value for a.
+-- Output is returned as list of ints.
+run :: Instructions -> Integer -> [Integer]
+run instructions a = go (a, 0, 0, 0)
   where
     go (a, b, c, ip) = case opcode of
         Just 0  -> go (a `div` (2 ^ comboOperand), b, c, ip')
@@ -20,7 +21,7 @@ run instructions = go
         Just 2  -> go (a, comboOperand `mod` 8, c, ip')
         Just 3  -> if   a == 0
                    then go (a, b, c, ip')
-                   else go (a, b, c, operand)
+                   else go (a, b, c, fromInteger operand)
         Just 4  -> go (a, b `xor` c, c, ip')
         Just 5  -> comboOperand `mod` 8 : go (a, b, c, ip')
         Just 6  -> go (a, a `div` (2 ^ comboOperand), c, ip')
@@ -37,14 +38,25 @@ run instructions = go
             | operand == 6          = c
 
 
-
 main :: IO ()
 main = do
     -- Part 1
     -- Not doing any automatic parsing today, since the input is tiny.
-    let state0       = (17323786, 0, 0, 0)
     let instructions = [2,4,1,1,7,5,1,5,4,1,5,5,0,3,3,0]
-    print $ run instructions state0
+    print $ run instructions 17323786
+
+
+    -- Part 2: Key insight is to individually assess blocks of three bits, i.e.
+    -- numbers from 0..7. I manually tried a BFS multiple times, but must have
+    -- made some errors along the way every time. However, this gave me the bounds
+    -- 164278764924544 < soln < 164281784823485, which we can easily bruteforce.
+    -- This yielded 164278764924605, which thankfully was right :D.
+
+    print $ fst
+          . head
+          . filter ((==instructions) . snd)
+          . map (\ i -> (i, run instructions i))
+          $ [164278764924544..164281784823485]
 
 
 
