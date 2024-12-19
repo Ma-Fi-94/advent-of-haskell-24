89ee466f62f278c3fc0902d8bed9b1147e8d05c5
Matthias M. Fischer <33027321+Ma-Fi-94@users.noreply.github.com>
2024-12-19 22:50:01 +0100

    Hell yea!

--- day19/Main.hs
+++ day19/Main.hs
@@ -2,8 +2,7 @@ module Main where
 
 import Data.Function (on)
 import Data.List (isPrefixOf, nubBy)
-import Utils (tok, snd3, thi3, nubByWith)
-import Debug.Trace (trace)
+import Utils (tok, fst3, snd3, thi3, nubByWith)
 
 -- Sugar
 type Towel  = String
@@ -31,7 +30,7 @@ isPossible towels design = go [([], design)]
         | null parses             = False
         | any (null . snd) parses = True
         | otherwise               = go
-                                  . nubBy ((==) `on` snd)
+                                  . nubBy ((==) `on` fst)
                                   . map (\ (towel, prefix, rest) ->
                                         (prefix ++ towel, drop (length towel) rest))
                                   $ [(towel, prefix, rest)
@@ -40,13 +39,43 @@ isPossible towels design = go [([], design)]
                                           towel `isPrefixOf` rest]
 
 
+
+
+-- Count the number of different ways a given design can be produced.
+-- Like before, we want to keep the lists small, so we "merge" elements
+-- with the same prefix. However, every list elements carries a counter,
+-- and upon merging entries, the counters have to be added.
+nbPossibilities :: [Towel] -> Design -> Int
+nbPossibilities towels design = go [([], design, 1)]
+  where
+    go parses
+        | null parses              = 0
+        | any (null . snd3) parses = sum (map thi3 (filter (null . snd3) parses))
+                                   + go (filter (not . null . snd3) parses)
+        | otherwise                = go
+                                   . nubByWith
+                                         ((==) `on` fst3)
+                                         (\ tuples -> (fst3 (head tuples),
+                                                       snd3 (head tuples),
+                                                       sum (map thi3 tuples))) 
+                                   . map (\ (towel, prefix, rest, count) ->
+                                         (prefix ++ towel, drop (length towel) rest, count))
+                                   $ [(towel, prefix, rest, count)
+                                         | towel                 <- towels,
+                                           (prefix, rest, count) <- parses,
+                                           towel `isPrefixOf` rest] 
+
+
 main :: IO ()
 main = do
     fileContents          <- readFile "input.txt"
     let (towels, designs) = parseInput fileContents
-
     print $ length
           . filter (isPossible towels)
+          $ designs
+
+    print $ sum
+          . map (nbPossibilities towels)
           $ designs
 
     print $ "Done."
